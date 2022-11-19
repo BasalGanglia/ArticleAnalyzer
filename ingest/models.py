90a47ed68edb10d5a8bf505196c455aaae8aaae9
Ilkka Kosunen <ilkka.kosunen@gmail.com>
2022-11-19 10:33:55 +0000

    end of sprint 4

--- ingest/models.py
+++ ingest/models.py
@@ -3,3 +3,23 @@ from typing import Any, Dict, List, Tuple
 
 from pydantic import BaseModel
 
+
+class Post(BaseModel):
+    content: str
+    publication: str
+
+class ProcessedPost(BaseModel):
+    publication: str
+    entities: Counter = Counter()
+    article_count: int = 0
+
+    @property
+    def pub_key(self) -> str:
+        return None
+    
+    def transform_for_database(self, top_n=2000) -> List[Tuple[str, str, str, Dict]]:
+        return None
+
+    def __add__(self, other) -> ProcessedPost:
+        return self
+ 
---
+++ ingest/shutdownwatcher.py
@@ -0,0 +1,25 @@
+import signal
+import time
+from .debugging import app_logger as log
+
+
+class ShutdownWatcher:
+
+    def __init__(self):
+        self.should_continue = True
+
+        for s in [signal.SIGTERM, signal.SIGINT]:
+            signal.signal(s, self.exit)
+        
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, *args, **kwargs):
+        self.exit()
+    
+    def serve_forever(self):
+        while self.should_continue:
+            time.sleep(0.1)
+
+    def exit(self,*args, **kwargs):
+        self.should_continue = False
